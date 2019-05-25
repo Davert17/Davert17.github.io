fc7114aa70e6eb01bcc96dad4dccc67d281f921b
Davert17 <51029931+Davert17@users.noreply.github.com>
2019-05-25 22:41:09 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -17,8 +17,8 @@ function ajaxGetSpoiler1(){
       document.getElementById("getspoiler1").innerHTML=xhttp.responseText;
       
     }
-    xhttp.open("POST","spoiler1.txt",true);
-    xhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhttp.open("GET","spoiler1.txt",true);
+    //xhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     xhttp.send();
 }
 
