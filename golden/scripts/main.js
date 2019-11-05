a305297c87ff46f6c9e8d2a6d3feb63f0fb5e3a9
Davert17 <51029931+Davert17@users.noreply.github.com>
2019-11-05 21:09:58 +0200

    Update main.js

--- golden/scripts/main.js
+++ golden/scripts/main.js
@@ -3,14 +3,16 @@ window.addEventListener('load',(e)=>{
     document.querySelector('#menu-toggle').addEventListener('click',openMenu);
     document.querySelector('#menu-toggle').addEventListener('touch',openMenu);
     function openMenu(e){
+        document.body.style.position='fixed';
         e.currentTarget.style.opacity='0';
         document.querySelector('.hidden-menu').style.top="0";
-        document.body.style.position='fixed';
+        
     }
     function closeMenu(e){
+        document.body.style.position='';
         document.querySelector('#menu-toggle').style.opacity='1';
         document.querySelector('.hidden-menu').style.top="-100%";
-        document.body.style.position='';
+        
     }
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('click',closeMenu);
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('touch',closeMenu);
