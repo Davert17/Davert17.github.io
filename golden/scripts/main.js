881f0929623f05e83c1fc51c42feb40464c3acab
Davert17 <51029931+Davert17@users.noreply.github.com>
2019-11-05 20:47:10 +0200

    Add files via upload

---
+++ golden/scripts/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',(e)=>{
+    
+    document.querySelector('#menu-toggle').addEventListener('click',openMenu);
+    document.querySelector('#menu-toggle').addEventListener('touch',openMenu);
+    function openMenu(e){
+        e.currentTarget.style.opacity='0';
+        document.querySelector('.hidden-menu').style.top="0";
+        document.body.style.overflow='hidden';
+    }
+    function closeMenu(e){
+        document.querySelector('#menu-toggle').style.opacity='1';
+        document.querySelector('.hidden-menu').style.top="-100%";
+        document.body.style.overflow='';
+    }
+    document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('click',closeMenu);
+    document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('touch',closeMenu);
+
+    for(let a of document.querySelectorAll('.hidden-menu a')){
+        a.addEventListener('touch',closeMenu);
+        a.addEventListener('click',closeMenu);
+    }
+})
