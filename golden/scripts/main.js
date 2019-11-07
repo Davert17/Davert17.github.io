4705d78b984fa0333def73efe892e0a758016b29
Davert17 <51029931+Davert17@users.noreply.github.com>
2019-11-07 17:51:39 +0200

    Add files via upload

--- golden/scripts/main.js
+++ golden/scripts/main.js
@@ -6,21 +6,14 @@ window.addEventListener('load',(e)=>{
         document.querySelector('body').style.height='100%';
 
         document.querySelector('body').style.overflow='hidden';
-        // document.querySelector('body').style.position='relavite';
+        document.querySelector('body').style.position='relavite';
 
-        // document.querySelector('html').style.height='100%';
+        document.querySelector('html').style.height='100%';
 
-        // document.querySelector('html').style.overflow='hidden';
-        // document.querySelector('html').style.position='relavite';
+        document.querySelector('html').style.overflow='hidden';
+        document.querySelector('html').style.position='relavite';
 
-        document.addEventListener(
-            'touchmove',
-            (e)=>{e.preventDefault();}
-            );
-            // document.addEventListener(
-            //     "scroll",
-            //     stopScroll
-            //     );
+        
         document.addEventListener("scroll",stopScroll);
         e.currentTarget.style.opacity='0';
         document.querySelector('.hidden-menu').style.top="0";
@@ -32,25 +25,20 @@ window.addEventListener('load',(e)=>{
         
         document.querySelector('#menu-toggle').style.opacity='1';
         document.querySelector('.hidden-menu').style.top="-100%";
-        // document.body.style.position='';
+        
         document.querySelector('body').style.height='';
-        // document.querySelector('body').style.position='';
-        // document.querySelector('body').style.overflow='';
+        document.querySelector('body').style.position='';
+        document.querySelector('body').style.overflow='';
 
-        // document.querySelector('html').style.height='';
+        document.querySelector('html').style.height='';
 
-        // document.querySelector('html').style.overflow='';
-        // document.querySelector('html').style.position='';
+        document.querySelector('html').style.overflow='';
+        document.querySelector('html').style.position='';
 
 
-        document.removeEventListener(
-            'touchmove',
-            stopScroll
-            );
+
     }
-    function stopScroll(e){
-        e.preventDefault();
-    }
+    
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('click',closeMenu);
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('touch',closeMenu);
 
