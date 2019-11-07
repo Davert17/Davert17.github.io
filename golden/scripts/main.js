6b9a0dfa2fcf380803b7cd60bd3b38888d5f39ee
Davert17 <51029931+Davert17@users.noreply.github.com>
2019-11-07 17:16:14 +0200

    Add files via upload

--- golden/scripts/main.js
+++ golden/scripts/main.js
@@ -4,9 +4,10 @@ window.addEventListener('load',(e)=>{
     document.querySelector('#menu-toggle').addEventListener('touch',openMenu);
     function openMenu(e){
         
-        document.querySelector('html').style.height='100%';
-        document.querySelector('html').style.overflow='hidden';
-
+        document.querySelector('body').style.height='100%';
+        document.querySelector('body').style.position='relavite';
+        document.querySelector('body').style.overflow='hidden';
+        document.addEventListener("scroll",stopScroll);
         e.currentTarget.style.opacity='0';
         document.querySelector('.hidden-menu').style.top="0";
         // setTimeout(()=>{document.body.style.position='fixed'},1000);
@@ -17,11 +18,15 @@ window.addEventListener('load',(e)=>{
         document.querySelector('#menu-toggle').style.opacity='1';
         document.querySelector('.hidden-menu').style.top="-100%";
         // document.body.style.position='';
-        document.querySelector('html').style.height='';
-        document.querySelector('html').style.overflow='';
+        document.querySelector('body').style.height='';
+        document.querySelector('body').style.position='';
+        document.querySelector('body').style.overflow='';
 
 
 
+    }
+    function stopScroll(e){
+        e.preventDefault();
     }
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('click',closeMenu);
     document.querySelector('.hidden-menu>#remove-menu>img').addEventListener('touch',closeMenu);
